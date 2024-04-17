118e2754be0a25f018086d8e68505d3fe63d0dca
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-04-17 13:03:17 -0600

    valor

---
+++ valores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    int *p;
+    p = &x;
+    printf("el valor de x es %d" , &x);
+    //y=*p+3;
+    //printf("el valor de y es %d" , y);
+    return 0;
+}
